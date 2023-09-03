4e5a17c5871949799d85db6a11f2bab83ce81a06
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-09-03 09:21:06 +0300

    Remove some default serializers because I'm not gonna want to serialize something if I don't even know what the heck it does and if someone wants to, then they can add the serializer in their config class

--- src/main/java/top/offsetmonkey538/monkeyconfig538/serializer/defaultSerializers/RegistrySerializer.java
+++ src/main/java/top/offsetmonkey538/monkeyconfig538/serializer/defaultSerializers/RegistrySerializer.java
@@ -3,8 +3,8 @@ package top.offsetmonkey538.monkeyconfig538.serializer.defaultSerializers;
 import blue.endless.jankson.JsonElement;
 import blue.endless.jankson.JsonNull;
 import blue.endless.jankson.api.Marshaller;
-import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registry;
 import top.offsetmonkey538.monkeyconfig538.serializer.ConfigSerializer;
 
 import static top.offsetmonkey538.monkeyconfig538.MonkeyConfig538.*;
